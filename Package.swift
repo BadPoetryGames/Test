14f57a42f7e9731c2f7a525ba2a23741c698b74c
jalingo <jameslingo@me.com>
2022-11-10 22:58:12 -0300

    TP-0 [James] moved test target to regular targets, renamed Test -> TestForSource and Tests -> TestForTests

--- Package.swift
+++ Package.swift
@@ -8,11 +8,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Test",
-            targets: ["Test"]),
+            name: "TestForSource",
+            targets: ["TestForSource"]),
         .library(
-            name: "Tests",
-            targets: ["Tests"]),
+            name: "TestForTests",
+            targets: ["TestForTests"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Test",
+            name: "TestForSource",
             dependencies: []),
-        .testTarget(
-            name: "Tests",
-            dependencies: ["Test"]),
+        .target(
+            name: "TestForTests",
+            dependencies: ["TestForSource"]),
     ]
 )
